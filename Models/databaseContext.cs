6ae4208769c36a955768c4797588e61e5fd9f04e
bryankroesbeek <BryanK.B96@gmail.com>
2018-01-20 21:42:41 +0100

    Added basis for a database connection

---
+++ Models/databaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BryankroesbeekNl.Models
+{
+    public partial class databaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite(@"Filename=./dbname");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { }
+    }
+}
