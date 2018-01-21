e543719a936a60a56154e68ae954a5dafc75ef41
bryankroesbeek <BryanK.B96@gmail.com>
2018-01-22 00:13:37 +0100

    Moved page to a new file and made improvements

--- Client/router.tsx
+++ Client/router.tsx
@@ -2,19 +2,9 @@ import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
 
-type Post = {
-    id: number
-    title: string
-    content: string
-    createdDate: Date
-    updatedDate: Date
-}
+import { Info } from './Components/Info'
 
-type State = {
-    posts: Post[] | "loading"
-}
-
-export class MainRouter extends React.Component<{}, State>{
+export class MainRouter extends React.Component<{}, {}>{
     render() {
         return (
             <BrowserRouter>
@@ -25,77 +15,4 @@ export class MainRouter extends React.Component<{}, State>{
             </BrowserRouter>
         )
     }
-}
-
-class Info extends React.Component<{}, State>{
-    constructor(props: {}) {
-        super(props)
-
-        this.state = {
-            posts: "loading"
-        }
-    }
-
-    componentWillMount() {
-        this.getResources();
-    }
-    componentDidUpdate() {
-        this.getResources();
-    }
-
-    getResources() {
-        if (this.state.posts !== "loading") return
-
-        fetch("api/infoblock")
-            .then(r => r.json())
-            .then(res => res as Post[])
-            .then(res => this.setState({ posts: res }))
-    }
-
-    render() {
-        return <div>
-            {this.renderpostform()}
-            {this.renderposts()}
-        </div>
-    }
-
-    content: string
-    title: string
-
-    renderpostform() {
-        return (
-            <div className="poop">
-                <form onSubmit={e => {
-                    e.preventDefault()
-                    let headers = new Headers()
-                    headers.append('content-type', 'application/json')
-                    fetch("api/infoblock", {
-                        method: "POST", headers: headers, body: JSON.stringify({
-                            Title: this.title,
-                            Content: this.content
-                        })
-                    })
-                        .then(() => {
-                            this.setState({ posts: "loading" })
-                        })
-                }} >
-                    <input type="text" onChange={e => this.title = e.target.value} />
-                    <input type="text" onChange={t => this.content = t.target.value} />
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        )
-    }
-
-    renderposts() {
-        return this.state.posts == "loading" ? <div>LOADING</div> :
-            <div>
-                {this.state.posts.map(p => <div key={p.id} className="poop">
-                    <div>{p.id}</div>
-                    <div>{p.title}</div>
-                    <div>{p.content}</div>
-                </div>)}
-            </div>
-    }
-
 }
