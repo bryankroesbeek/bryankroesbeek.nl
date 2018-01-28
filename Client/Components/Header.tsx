ccf330cf7a0ecf50abc88c6140a373f0044c3d2e
bryankroesbeek <BryanK.B96@gmail.com>
2018-01-28 14:44:52 +0100

    Improved header

--- Client/Components/Header.tsx
+++ Client/Components/Header.tsx
@@ -22,9 +22,9 @@ export let Header = (
                         {props.links.map((link, int) =>
                             <Link className="nav__link" to={link.url} key={`nav_${int}`} >
                                 <span className="nav__item">
-                                    <label className="item__label">
+                                    <span className="item__label">
                                         {link.title}
-                                    </label>
+                                    </span>
                                 </span>
                             </Link>
                         )}
