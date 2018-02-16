58f8894d7ef1cb183f2fe6666a66cc8db66f0af9
bryankroesbeek <BryanK.B96@gmail.com>
2018-02-16 18:23:57 +0100

    Footer is now a footer

--- Client/Components/Footer.tsx
+++ Client/Components/Footer.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 
-let Header = (
+export let Footer = (
     props: {}
 ) => (
     <footer>
-        
+        ...
     </footer>
 )
