184282d5833a238639a3fd7fa42796f01120c868
bryankroesbeek <BryanK.B96@gmail.com>
2018-02-21 23:37:44 +0100

    Added a quote with styling

--- Client/Components/Home/Home.tsx
+++ Client/Components/Home/Home.tsx
@@ -3,11 +3,19 @@ import * as ReactDom from 'react-dom'
 import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
 
 export class Home extends React.Component<{}, {}>{
-    render(){
+    render() {
         return (
-            <div className="content">
-                <div>
-                    Hello
+            <div>
+                <div className="quote">
+                    <div className="quote-background">
+                        <div className="quote-image-wrapper">
+                            <img className="quote-image" src="images/pattern.svg" />
+                        </div>
+                        <div className="quote-content">
+                            <h1 className="quote-text">I used to have an open mind but my brains kept falling out</h1>
+                            <h3 className="quote-author">- Bryan Kroesbeek</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
