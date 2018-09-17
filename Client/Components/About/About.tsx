ab2f16549d7116c83f40ba23c1587f636e070a09
bryankroesbeek <bryank.b96@gmail.com>
2018-09-17 21:06:56 +0200

    Fixed some spelling mistakes

--- Client/Components/About/About.tsx
+++ Client/Components/About/About.tsx
@@ -16,26 +16,26 @@ export class About extends React.Component<{}, {}>{
 
                 <p>
                     Naast mijn opleiding ben ik ook werkzaam als Trainee Webdevelopment bij internetbureau <a href="https://hoppinger.com">Hoppinger</a>.
-                    Ik ben voor het eerst in aanraking gekomen met dit bedrijf toen ik ging solliciteren voor een stageplek bij het bedrijf.
+                    Ik ben voor het eerst in aanraking gekomen met dit bedrijf toen ik ging solliciteren voor een stageplek.
                     <br/>
-                    Tijdens mijn stage periode heb ik aan verschillende projecten gewerkt waardoor ik mijn programmeer vaardigheden heb kunnen verbeteren.
-                    Voordat ik stage ging lopen wist ik nog weinig af van webdevelopment in het algemeen.
+                    Tijdens mijn stageperiode heb ik aan verschillende projecten gewerkt, hierdoor heb ik mijn programmeervaardigheden kunnen verbeteren.
+                    Voordat ik stage ging lopen wist ik nog weinig af van webdevelopment.
                     Ik had nog nooit gewerkt met programmeertalen en frameworks zoals TypeScript en React. Deze technieken heb ik volledig tot me kunnen nemen.
                 </p>
 
                 <p>
-                    Uiteraard heb ik ook nog hobby's waar ik zoal mee bezig houd. Eén van de grootste hobby's is het spelen van video games. 
+                    Uiteraard heb ik ook nog hobby's waar ik me mee bezig houd. Eén van de grootste hobby's is het spelen van video games. 
                     Zo kan ik geheel opgaan in het spelen van games. 
                     Games die ik nu vooral speel zijn {" "}
-                    <a href="https://www.rockstargames.com/V/">Grand Theft Auto 5</a> en <a href="https://www.rocketleague.com/">Rocket League</a>.
+                    <a href="https://www.projectcarsgame.com/?lang=en-us">Project CARS 2</a> en <a href="https://www.rocketleague.com/">Rocket League</a>.
                 </p>
 
                 <p>
                     Daarnaast programmeer ik uiteraard in mijn vrije tijd ook nog. 
-                    Ik me dan bezig met projecten die me vooral leuk lijken, maar ook projecten waar ik nieuwe dingen van kan leren.
+                    Ik houd me dan bezig met projecten die mij leuk lijken, maar ook projecten waar ik nieuwe dingen van kan leren.
                 </p>
                 <p>
-                    De projecten waar ik me bezig houdt of mee bezig heb gehouden zijn te vinden onder <Link to="/projects">Projecten</Link>
+                    De projecten waar ik mij mee bezig houd of mee bezig heb gehouden zijn te vinden onder <Link to="/projects">Projecten</Link>
                 </p>
             </div>
         </>)
