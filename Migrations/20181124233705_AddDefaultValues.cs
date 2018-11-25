2ecf27c344d272b174791f8c4c4d67230b35f217
bryankroesbeek <bryank.b96@gmail.com>
2018-11-25 02:35:33 +0100

    Add default values to database format

---
+++ Migrations/20181124233705_AddDefaultValues.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BryankroesbeekNl.Migrations
+{
+    public partial class AddDefaultValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Project",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true,
+                oldDefaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Project",
+                type: "text",
+                nullable: true,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Database/BryankroesbeekNlContext.cs
+++ Models/Database/BryankroesbeekNlContext.cs
@@ -19,15 +19,21 @@ namespace BryankroesbeekNl.Models.Database
 
                 entity.Property(e => e.Link)
                     .IsRequired()
-                    .HasMaxLength(255);
+                    .HasMaxLength(255)
+                    .HasDefaultValue("");
 
                 entity.Property(e => e.Name)
                     .IsRequired()
-                    .HasMaxLength(100);
+                    .HasMaxLength(100)
+                    .HasDefaultValue("");
 
-                entity.Property(e => e.Position).HasColumnType("int(11)");
+                entity.Property(e => e.Position)
+                    .HasColumnType("int(11)")
+                    .HasDefaultValue(0);
 
-                entity.Property(e => e.Visible).HasColumnType("bit(1)");
+                entity.Property(e => e.Visible)
+                    .HasColumnType("bit(1)")
+                    .HasDefaultValue(false);
             });
 
             modelBuilder.Entity<AuthToken>(entity =>
@@ -42,7 +48,7 @@ namespace BryankroesbeekNl.Models.Database
                     .HasMaxLength(255);
 
                 entity.Property(e => e.CreatedAt).HasColumnType("bigint(20)");
-                
+
                 entity.Property(e => e.ExpiresAt).HasColumnType("bigint(20)");
             });
         }
