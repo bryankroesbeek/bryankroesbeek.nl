4956948db06278c15a0fcbe09342273b2ba98647
bryankroesbeek <bryank.b96@gmail.com>
2019-02-16 10:33:10 +0100

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,23 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gsass = require('gulp-sass');
 var concat = require('gulp-concat');
 var cleanCss = require('gulp-clean-css');
 
 var cssPath = 'Client/stylesheets/**/*.css';
 var sassPath = 'Client/stylesheets/**/*.scss';
 
-gulp.task('css', () => {
-    return gulp.src(cssPath)
-        .pipe(cleanCss())
-        .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('wwwroot/css/'))
-});
-
-gulp.task('sass', () => {
+function sass() {
     return gulp.src(sassPath)
-        .pipe(sass())
+        .pipe(gsass())
         .pipe(cleanCss())
         .pipe(concat('style.min.css'))
         .pipe(gulp.dest('wwwroot/css/'))
-});
+};
 
-gulp.task('watch', () => {
-    gulp.watch([cssPath, sassPath], ['css', 'sass']);
-});
+function watch() {
+    gulp.watch([cssPath, sassPath], sass);
+};
 
-gulp.task('default', ['css', 'sass']);
+exports.sass = sass;
+exports.watch = watch;
+exports.default = sass;
